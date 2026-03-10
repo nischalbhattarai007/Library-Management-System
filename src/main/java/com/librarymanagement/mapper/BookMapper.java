90da4715224df9d7ffeccc0fb87064449c5cea23
nischalbhattarai007 <nischalbhattarai345@gmail.com>
2026-03-10 11:13:41 +0545

    create a mapper class for books to make code cleaner shorter and more readable

---
+++ src/main/java/com/librarymanagement/mapper/BookMapper.java
@@ -0,0 +1,41 @@
+package com.librarymanagement.mapper;
+
+import com.librarymanagement.dto.BookDto;
+import com.librarymanagement.entity.BookEntity;
+
+public class BookMapper {
+    /*
+  Convert BookEntity -> BookDto
+  Used when sending data to client
+ */
+    public static BookDto toDto(BookEntity entity){
+        if(entity==null){
+            return null;
+        }
+        return new BookDto(
+                entity.getBookId(),
+                entity.getBookTitle(),
+                entity.getGenre(),
+                entity.isLent(),
+                entity.getLentTo(),
+                entity.getAuthor() !=null ? entity.getAuthor().getAuthorName():null
+        );
+    }
+       /*
+     Convert BookEntity -> BookDto
+     Used when sending data to client
+    */
+    public static BookEntity toEntity(BookDto toDto){
+        if(toDto==null){
+            return null;
+        }
+        BookEntity entity=new BookEntity();
+        entity.setBookId(toDto.getBookId());
+        entity.setBookTitle(toDto.getBookTitle());
+        entity.setGenre(toDto.getGenre());
+        entity.setLent(toDto.isLent());
+        entity.setLentTo(toDto.getLentTo());
+        return entity;
+    }
+
+}
